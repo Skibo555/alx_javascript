1a69444c553a6ae3e23c551f874707d8e5dbe5ad
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-15 05:18:59 +0100

    Js, What the fuck is this, it's weird

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
-let c = "C is fun";
-let python = "Python is cool";
+let c = "C is fun \n";
+let python = "Python is cool \n";
 let javaScript = "JavaScript is amazing"
 console.log(c, python, javaScript)
