341a0c18ee5f8269a462ac2960b6db1bd9d8dbcf
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-19 08:54:01 +0100

    I did it bvruvh

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+        // a constructor in js
+        if (w >= 0 && h >= 0) {
+            this.width = w;
+            this.height = h;
+        } else {
+            this.width = 0;
+            this.height = 0;
+        }
+    }
+
+    print() {
+        if (this.width === 0 || this.height === 0) {
+            console.log("Empty object");
+        } else {
+            for (let i = 0; i < this.height; i++) {
+                console.log('X'.repeat(this.width));
+            }
+        }
+    }
+    rotate() {
+        [this.width, this.height] = [this.height, this.width]
+    }
+    double() {
+        this.width *= 2;
+        this.height *= 2;
+        return this;
+    };
+}
+
+
+const obj = new Rectangle();
+
+module.exports = Rectangle;
