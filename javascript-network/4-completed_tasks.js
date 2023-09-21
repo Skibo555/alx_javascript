e0d5c94a2a18350a09ed9aaeecf2084aa099d08f
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 22:49:42 +0100

    File system is very cool

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 
-const { userInfo } = require('os');
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const completedTask = {};
+
 
 request.get(apiUrl, function (error, response, body) {
     const members = JSON.parse(body);
+    const completedTask = {};
     members.forEach((member) => {
         if (member.completed) {
-            if (completedTask[userId]) {
-                completedTask[userId]++;
+            if (completedTask[member.userId]) {
+                completedTask[member.userId]++;
             } else {
-                completedTask[userId] = 1;
+                completedTask[member.userId] = 1;
             }
         }
-    })
+    });
     const members_completed_tasks = {};
     for (const userId in completedTask) {
         members_completed_tasks[userId] = completedTask[userId]
-    }
+    };
     console.log(members_completed_tasks);
-})
+});
