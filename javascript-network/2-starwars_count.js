b0bd7b54713ce6ecbe09850039449b56f4949d82
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 20:37:08 +0100

    This is awesome with backticks

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -4,12 +4,12 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url, function (response, body) {
+request.get(url, function (error, response, body) {
     const films = JSON.parse(body);
-    const actor = films.results.filter((film) =>
+    const character = films.results.filter((film) =>
         film.characters.some((characterUrl) =>
             characterUrl.endsWith('/18/'))
     );
-    const counter = actor.length;
+    const counter = character.length;
     console.log(counter);
 });
