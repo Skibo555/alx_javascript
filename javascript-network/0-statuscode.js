9d5bd60d2f31ed3b054dd3ebe77513bb49551fe7
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 09:14:34 +0100

    Working on the GET

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -2,7 +2,7 @@
 const { STATUS_CODES } = require('http');
 const request = require('request');
 
-if (process.argv.length >= 2 {
+if (process.argv.length >= 2) {
     const url = process.argv[2];
     req = request.get(url, 'code:' + { STATUS_CODES })
 }
