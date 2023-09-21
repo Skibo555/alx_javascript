810d12121031435eade9c88aa1e8a227af156537
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 09:41:27 +0100

    Working on the GET

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const https = require('https');
+const http = require('http');
 const request = require('request');
 
 if (process.argv.length >= 2) {
     const url = process.argv[2];
-    https.get(url, (response) => {
+    http.get(url, (response) => {
         const statusCode = response.statusCode;
         console.log('code:' + { statusCode })
     })
