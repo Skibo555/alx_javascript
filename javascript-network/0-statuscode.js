41b959801d7a6b70c4831f2af093881b6702f967
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 10:03:53 +0100

    We are here

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const http = require('http');
 const request = require('request');
 
 if (process.argv.length >= 2) {
     const url = process.argv[2];
-    http.get(url, (response) => {
-        const statusCode = response.statusCode;
-        console.log('code:' + { statusCode })
-    })
+    request.get(url)
+        .on('response', response => (console.log('code:' + response.statusCode)))
 };
