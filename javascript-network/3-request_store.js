8130f363bb41eebd815c3ac940495c9a42e8b16c
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 21:13:54 +0100

    File system is very cool

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+
+
 const request = require('request');
 const fs = require('fs')
 
 url = process.argv[2];
 path = process.argv[3];
 
-request.get(url).pipe(fs.createReadStream(path));
+request.get(url).pipe(fs.createWriteStream(path));
