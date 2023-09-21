c98d1e41f193db8c837722153ad6b5439abbee5f
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 11:06:59 +0100

    This is awesome with backticks

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -7,6 +7,6 @@ if (process.argv.length !== 3) {
 }
 const id = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${id}`;
-request.get(apiUrl, (body));
-const movieData = JSON.parse(body);
+req = request.get(apiUrl, (body));
+const movieData = JSON.parse(req);
 console.log(movieData.title);
