c578b96faceae8e3142060c42157b5d864378d63
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 22:54:15 +0100

    File system is very cool

---
+++ javascript-network/5-starwars_characters.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+id = process.argv[2];
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + id;
+const actors = [];
+
+request.get(apiUrl, (error, response, body) => {
+    if (response.statusCode !== 200) {
+        console.error('Request failed with status code:', response.statusCode);
+        return;
+    }
+
+    try {
+        // Parse the JSON response
+        const data = JSON.parse(body);
+        actors.push(data.characters);
+    } catch (e) {
+        console.error('Error parsing JSON:', e);
+    }
+
+    actors[0].forEach(function (actor) {
+        request.get(actor, (error, response, body) => {
+            if (error) {
+                console.error('Error:', error);
+                return;
+            }
+
+            if (response.statusCode !== 200) {
+                console.error('Request failed with status code:', response.statusCode);
+                return;
+            }
+
+            try {
+                // Parse the JSON response
+                const data = JSON.parse(body);
+                console.log(data.name);
+            } catch (e) {
+                console.error('Error parsing JSON:', e);
+            }
+        })
+    })
+});
