922fb40c5bac0b523df3d1b119876672834cd2da
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 20:22:14 +0100

    This is awesome with backticks

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -2,16 +2,14 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-    process.exit(1);
-}
-const id = 18;
 const url = process.argv[2];
 
-request.get(apiUrl, (response, body));
-const filmData = JSON.parse(body);
-
-const films = filmData.results.filter((film) =>
-    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)
-);
-console.log(films)
+request.get(url, function (response, body) {
+    const filmData = JSON.parse(body);
+    const actor = filmData.results.filter((film) =>
+        film.characters.some((characterUrl) =>
+            characterUrl.endsWith('/18/'))
+    );
+    const counter = actor.length;
+    console.log(number);
+});
