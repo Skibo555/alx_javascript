8ee5c0074b4ad5b80420f0382a9ffb4d7c690c0a
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-21 10:15:36 +0100

    I wanted to use request but it would not work

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 if (process.argv.length >= 2) {
     const url = process.argv[2];
     request.get(url)
-        .on('response', response => (console.log('code:' + response.statusCode)))
+        .on('response', response => (console.log('code:' + ' ' + response.statusCode)))
 };
